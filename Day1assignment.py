8b804c6b322a4c2d5397015cdf3e9cecba54a271
nasreen banu <nasreen.1.banu@nokia.com>
2024-09-20 15:41:04 +0530

    Day1assignment

---
+++ Day1assignment.py
@@ -0,0 +1,21 @@
+x=1
+y= 1.0
+z='1'
+print("type of x is :",type(x),"type of y= is :", type(y),"type of z is :" ,type(z))
+
+a=b=c=5
+print("value of a:",a,"value of b:",b, "value of c:", c)
+
+
+m=5;n=10
+print("sum of M+N is",(m+n))
+
+fruits = ["apple", "banana", "cherry", "date", "elderberry"]
+print("second fruit in the list is:",fruits[1], "fourth fruit in the list is:",fruits[3])
+
+
+numbers = [10, 20, 30, 40, 50]
+numbers[2]=35
+print("new list:",numbers)
+
+new_tuple=("red", "green", "blue")
