facb785b66ceb5aa8ce360e05578590968eaceda
nasreen banu <nasreen.1.banu@nokia.com>
2024-09-22 17:48:27 +0530

    Day1assignment

--- Day1assignment.py
+++ Day1assignment.py
@@ -19,3 +19,49 @@ numbers[2]=35
 print("new list:",numbers)
 
 new_tuple=("red", "green", "blue")
+print(new_tuple[2])
+
+
+new_tuple = new_tuple[:1] + ('yellow',) + new_tuple[2:]
+
+
+coordinates = (10, 20, 30)
+x,y,z=coordinates
+print(x,y,z)
+for blue in new_tuple:
+    if blue=="blue":
+        print("true")
+        break
+    else:
+        print(" Sorry")
+
+tuple_days = ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
+print(len(tuple_days))
+
+#dictionary
+
+D={"Alice": 85, "Bob": 90, "Charlie": 78}
+
+print(D['Bob'])
+#Add and Remove Key-Value Pairs:
+del D["Charlie"]
+D['david']=90
+print(D)
+
+#Update Dictionary Values:
+for update in D:
+    if update=="Bob":
+        D["Bob"]=95
+        break
+
+print(D)
+
+#Check if Key Exists in a Dictionary:
+for alice in D:
+    if D["Alice"]=="Alice":
+        print("yes its a key")
+    else: print("not a key")
+
+#Dictionary Length:
+length_D=len(D)
+print(length_D)
